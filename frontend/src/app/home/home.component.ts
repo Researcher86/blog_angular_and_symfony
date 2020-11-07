79383395b41f2282ec059ef30899ac0745837e04
Tanat <researcher2286@gmail.com>
2020-11-07 18:11:29 +0600

    Add home module

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+// import { ArticleListConfig, TagsService, UserService } from '../core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
